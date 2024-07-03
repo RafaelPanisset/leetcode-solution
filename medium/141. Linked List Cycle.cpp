ba44f875e3016210b5e3141bb9b69fe16357dcfa
Rafael Panisset Motta <51482528+RafaelPanisset@users.noreply.github.com>
2024-07-02 21:15:30 -0300

    Create 141. Linked List Cycle.cpp

---
+++ medium/141. Linked List Cycle.cpp
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/linked-list-cycle/
+//O(N)
+//O(1)
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        unordered_set<ListNode*> seen;
+        while (head != nullptr) {
+            if (seen.count(head)) {
+                return true;
+            }
+            seen.insert(head);
+            head = head->next;
+        }
+        return false;
+    }
+};
